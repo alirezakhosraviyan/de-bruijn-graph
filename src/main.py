6c37ef229f89fbab31fd1a1355b99b51c734240a
alireza <alireza.khosravian@coosto.com>
2023-01-15 16:00:56 +0100

    robustness value

--- src/main.py
+++ src/main.py
@@ -1,42 +1,9 @@
 import networkx as nx
-import matplotlib.pyplot as plt
-from itertools import product
+from lib.de_bruijn_graph import draw_de_bruijn_graph, create_de_bruijn_graph
+from lib.tools import get_fragility, get_robustness, get_random_graphe
 
+# the_graph = create_de_bruijn_graph(2, 5)
 
-def create_de_bruijn_graph(k, n):
-    g = nx.DiGraph()
-    for p in product(tuple(map(str, range(k))), repeat=n):
-        code = ''.join(p)
-        u = str(code).zfill(n)
-        tmp = u[1:]
-        for i in range(k):
-            v = tmp + str(i)
-            g.add_edge(u, v, weight=i)
-    return g
+# draw_de_bruijn_graph(the_graph)
 
-
-def de_bruijn_nx(k, n):
-    g = create_de_bruijn_graph(k, n)
-    ls = nx.eulerian_circuit(g)
-    res = ''.join(map(str, (g.get_edge_data(*e)['weight'] for e in ls)))
-    return res
-
-
-def draw_de_bruijn_graph(g):
-    plt.figure(figsize=(20,20), dpi=80)
-    nx.draw_networkx(
-        g, pos=nx.circular_layout(g),
-        node_shape='o', node_size=4000, font_size=20,
-        edge_color='#555555', width=3.0
-    )
-    nx.draw_networkx_edge_labels(
-        g, pos=nx.circular_layout(g),
-        edge_labels=nx.get_edge_attributes(g, 'weight'),
-        font_size=24, label_pos=0.25, rotate=False
-    )
-    plt.axis('off')
-    plt.show()
-
-
-draw_de_bruijn_graph(create_de_bruijn_graph(2, 3))
-
+print('res: ', get_robustness(get_random_graphe([1, 3, 4, 2, 3, 3])))
