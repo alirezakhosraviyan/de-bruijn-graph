374cb3db3449dd091c1eb93661bcd36955a2198d
alireza <alireza.khosravian@coosto.com>
2023-01-25 18:02:04 +0100

    new features added

--- src/lib/tools.py
+++ src/lib/tools.py
@@ -1,5 +1,11 @@
 import networkx as nx
+import scipy
+import random
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
 import matplotlib.pyplot as plt
+from .de_bruijn_graph import create_de_bruijn_graph
 
 
 def get_random_graphe(degree_sequence) -> nx.Graph:
@@ -9,21 +15,89 @@ def get_random_graphe(degree_sequence) -> nx.Graph:
     return G
 
 
+def get_adjacency_matrix(graph):
+    num_nodes = len(graph.nodes)
+    matrix = np.zeros((num_nodes, num_nodes))
+    node_index = {node: idx for idx, node in enumerate(graph.nodes)}
+    for source, destination in graph.edges:
+        source_index = node_index[source]
+        destination_index = node_index[destination]
+        matrix[source_index, destination_index] = 1
+    return matrix
+
+
 def get_fragility(graph):
     """returns the special value"""
-    adjacency_list = nx.adjacency_spectrum(graph)
-    return -max(adjacency_list)
+    m = nx.adjacency_matrix(graph)
+    eig_values, eig_vectors = scipy.linalg.eig(m.toarray())
+    return -max(np.abs(eig_values))
 
 
 def get_robustness(graph: nx.Graph):
-    """returns the robustness value"""
     temp_graph = graph.copy()
-    list_of_degrees = temp_graph.degree
-    sorted_list_of_degrees = sorted(list_of_degrees, key=lambda i: i[1], reverse=True)
-
+    degree_list = temp_graph.degree
+    degree_list_sorted = sorted(degree_list, key=lambda i: i[1], reverse=True)
     total_sizes = 0
-    for cur in sorted_list_of_degrees:
+    robustness = []
+    fragilities = []
+    for cur in degree_list_sorted:
         temp_graph.remove_node(cur[0])
-        total_sizes += temp_graph.size()
+        Rt = temp_graph.size() / len(degree_list_sorted)
+        robustness.append(Rt)
+        if temp_graph.edges:
+            total_sizes += len(max(nx.weakly_connected_components(temp_graph), key=len))
+            fragilities.append(get_fragility(temp_graph))
+    return total_sizes / len(degree_list_sorted), robustness, fragilities
 
-    return total_sizes / len(sorted_list_of_degrees)
+
+def show_linear_model():
+    np.polyfit()
+
+
+def monte_carlo(shape):
+    graph: nx.Graph = create_de_bruijn_graph(*shape)
+    if not graph:
+        return 0, 0, 0
+    nodes = list(graph.nodes)
+    initial_edge_size = graph.size()
+    random.shuffle(nodes)
+    connected = 0
+
+    for node in nodes:
+        graph.remove_node(node)
+        if graph and nx.is_weakly_connected(graph):
+            connected += 1
+        else:
+            break
+    p = connected / initial_edge_size
+    if graph:
+        R = len(max(nx.weakly_connected_components(
+            graph), key=len)) / initial_edge_size
+        F = get_fragility(graph)
+    else:
+        R = 0
+        F = 0
+    return p, F, R
+
+
+def fit_polynomial_and_draw(rfs):
+    if not rfs:
+        return
+    rfs.sort(key=lambda x: x[0])
+    pf = PolynomialFeatures(degree=2)
+    X = np.array([x[0] for x in rfs])
+    X = X.reshape(-1, 1)
+    y = np.array([x[1] for x in rfs])
+    X_poly = pf.fit_transform(X)
+    lin_reg = LinearRegression()
+    lin_reg.fit(X_poly, y)
+    plt.figure(figsize=(12, 10))
+    plt.scatter(X, y, c='blue')
+    plt.plot(X, lin_reg.predict(X_poly), color='red')
+    plt.xticks(color='white')
+    plt.yticks(color='white')
+    plt.xlabel('F', color='white')
+    plt.ylabel('R', color='white')
+    plt.show()
+
+
--- src/main.py
+++ src/main.py
@@ -1,9 +1,56 @@
-import networkx as nx
+import click
 from lib.de_bruijn_graph import draw_de_bruijn_graph, create_de_bruijn_graph
-from lib.tools import get_fragility, get_robustness, get_random_graphe
+from lib.tools import fit_polynomial_and_draw, get_robustness, monte_carlo
 
-# the_graph = create_de_bruijn_graph(2, 5)
 
-# draw_de_bruijn_graph(the_graph)
+@click.group()
+def run():
+    """main function for commands"""
 
-print('res: ', get_robustness(get_random_graphe([1, 3, 4, 2, 3, 3])))
+
+@click.command(name="show-de-bru")
+@click.option("-n", "--nodes", required=True, help="the number of nodes", type=int)
+@click.option("-e", "--edges", required=True, help="the number of edges", type=int)
+def show_de_bru(nodes, edges):
+    print(nodes, edges)
+    the_graph = create_de_bruijn_graph(nodes, edges)
+    draw_de_bruijn_graph(the_graph)
+
+
+@click.command(name="show-robustness")
+@click.option("-n", "--nodes", required=True, help="the number of nodes")
+@click.option("-e", "--edges", required=True, help="the number of edges")
+def show_robustness(nodes, edges):
+    the_graph = create_de_bruijn_graph(nodes, edges)
+    res = get_robustness(the_graph)
+
+    click.echo(f"Robustness: \n {res}")
+
+
+@click.command(name="simulate")
+@click.option("-n", "--nodes", required=True, help="the number of nodes", type=int)
+@click.option("-e", "--edges", required=True, help="the number of edges", type=int)
+@click.option("-s", "--simulation", required=True, help="the number of edges", type=int)
+def simulate(nodes, edges, simulation):
+    # by_probability represents the (F, R) tuple for 10 different cases of probability
+    # by_probability[0] corresponds to probability between 0.0 -> 0.1
+    # by_probability[1] corresponds to probability between 0.1 -> 0.2 and etc.
+    by_probability = [[] for _ in range(10)]
+    for epoch in range(simulation):
+        for n in range(2, 5):
+            for k in range(2, 5):
+                p, F, R = monte_carlo((k, n))
+                by_probability[int(p * 10)].append((F, R))
+        print(f'Epoch {epoch} has successfully completed!')
+
+    for rfs in by_probability:
+        fit_polynomial_and_draw(rfs)
+
+
+run.add_command(show_de_bru)
+run.add_command(show_robustness)
+run.add_command(simulate)
+
+
+if __name__ == '__main__':
+    run()
